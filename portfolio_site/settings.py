a26a1c2928d6dfa7e2c04a5e4f52c51ded5bec2f
Anas07032005 <anasulkamileen07@gmail.com>
2025-12-16 19:53:57 +0530

    Fix settings.py for render

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
